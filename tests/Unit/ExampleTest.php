738d0f8432e8a8e16d711bcec2d051969051d60d
GabrielHenriqueRibas <110944395+GabrielHenriqueRibas@users.noreply.github.com>
2024-10-21 20:57:48 -0300

    @gabrielribas/test (#1)
    * Add document Workflow and first test
    
    * first test
    
    * add test
    
    * Add Workflow
    
    * removing previous versions php workflow

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,4 +14,21 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
 
+    public function testSomaValores()
+    {
+        $resultado = 5 + 3;
+        $this->assertEquals(8, $resultado, "A soma de 5 e 3 deveria ser 8");
+    }
+
+    public function testPipelineCompleto()
+    {
+        $pipelineExecutado = true; 
+        $this->assertTrue($pipelineExecutado, "O pipeline deveria ser executado com sucesso");
+    }
+
+    public function testEtapaStatusPipeline()
+    {
+        $response = ['status' => 'success', 'data' => []];
+        $this->assertContains('success', $response, "O status do pipeline deveria ser 'success'");
+    }
 }
